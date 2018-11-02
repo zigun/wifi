135af6e69c6f83c83887cf9d367dc18014ebba61
zigun <zigun_x@ymail.com>
2018-11-02 19:57:19 +0700

    delete

--- include/config.php
+++ include/config.php
@@ -1,3 +1,5 @@
 <?php 
 if(substr($_SERVER["REQUEST_URI"], -10) == "config.php"){header("Location:./");}; 
 $data['mikhmon'] = array ('1'=>'mikhmon<|<mikhmon','mikhmon>|>aWNlbA==');
+
+$data['peta'] = array ('1'=>'peta!192.168.27.5','peta@|@admin','peta#|#qJammWNiaWk=','peta%Peta Hotspot','peta^peta.wifi','peta&Rp','peta*10','peta(1','peta)0','peta=0');
